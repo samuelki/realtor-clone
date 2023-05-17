cb97ea5542c79d5289ebb909455a6a78464563a4
Samuel Ki <samuelki62@gmail.com>
2023-05-17 12:27:43 -0700

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/offers' element={<Offers />} />
+        </Routes>
+      </Router>
     </>
   );
 }
