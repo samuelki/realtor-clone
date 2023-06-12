5eac4cf590a90392cafaa32c6de63ea772b7b44e
Samuel Ki <samuelki62@gmail.com>
2023-06-12 02:48:59 -0700

    add create listing page UI

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import ForgotPassword from './pages/ForgotPassword';
 import Offers from './pages/Offers';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
+import CreateListing from './pages/CreateListing';
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
           <Route path='/register' element={<Register />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/offers' element={<Offers />} />
+          <Route path='/create-listing' element={<CreateListing />} />
         </Routes>
       </Router>
       <ToastContainer
