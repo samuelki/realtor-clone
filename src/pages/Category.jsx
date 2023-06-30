9fa2a72d2bc6250aacdb0b3522e04febee62eb9b
Samuel Ki <samuelki62@gmail.com>
2023-06-30 14:24:43 -0700

    fix no current offers section

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -97,7 +97,10 @@ export default function Category() {
           )}
         </>
       ) : (
-        <p>There are no current offers</p>
+        <p>
+          There are no current{' '}
+          {params.categoryName === 'rent' ? 'places for rent' : 'places for sale'}
+        </p>
       )}
     </div>
   );
